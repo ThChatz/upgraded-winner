1189014ed62a081ba5d702a86427e918e4276cc3
vasilisp100 <vasilis.p.100@gmail.com>
2021-09-23 20:33:21 +0300

    added messages and notifications endpoints

---
+++ src/upgraded_winner/routes/messages.clj
@@ -0,0 +1,28 @@
+(ns upgraded-winner.routes.messages
+  (:require [hugsql.core :as hugsql]
+            [upgraded-winner.db :refer [ db ]]))
+
+(hugsql/def-sqlvec-fns "queries/messages.sql")
+(hugsql/def-db-fns "queries/messages.sql")
+
+(defn get-conv-handler [req]
+  (get-usr-convs db {:usr (-> req :session :identity)}))
+
+(defn get-messages-handler [req]
+  (get-conv-messages db {:usr (-> req :session :identity) 
+                        :conversation (-> req :parameters :path :conv-id)
+                        :page-num (-> req :parameters :path :page)}))
+
+(defn post-message [req]
+  (insert-message db {}))
+(def route
+  ["/conversations"
+   {:name ::conversations
+    :get
+    {:parameters {:path {:page pos-int?}}
+     :handler get-conv-handler}}]
+  ["/conversations/:conv-id/:page"
+   {:name ::conversation-messages
+    :get
+    {:parameters {:path {:page pos-int? :conv-id pos-int?}}
+     :handler get-messages-handler}}])
